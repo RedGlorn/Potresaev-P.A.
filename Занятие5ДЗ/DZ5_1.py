b0331ff355d010ed115d1130361148fcfe1f2ba8
RedGlorn <potresaevp@gmail.com>
2021-11-26 13:13:36 +0300

    ДЗ Занятие 5

---
+++ Занятие5ДЗ/DZ5_1.py
@@ -0,0 +1,8 @@
+#-- coding: utf-8 --
+print('Введите целое число N')
+N = int(input())
+i = 0
+while i <= N:
+	i = i**2
+	print(i)
+	i += 1
---
+++ Занятие5ДЗ/DZ5_4.py
@@ -0,0 +1,9 @@
+#-- coding: utf-8 --
+print('Введите числа x,y')
+x = int(input())
+y = int(input())
+i = 1
+while x < y:
+	x *= 1.1
+	i += 1
+print(i)
