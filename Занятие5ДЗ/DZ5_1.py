d053970a16f9b39d2d2f4e5bb1d1fd283ee4f7b5
RedGlorn <potresaevp@gmail.com>
2021-12-15 14:02:56 +0300

    Переделал номера 1,4

--- Занятие5ДЗ/DZ5_1.py
+++ Занятие5ДЗ/DZ5_1.py
@@ -1,8 +1,7 @@
 #-- coding: utf-8 --
 print('Введите целое число N')
 N = int(input())
-i = 0
-while i <= N:
-	i = i**2
-	print(i)
+i = 1
+while i**2 <= N:
+	print(i**2)
 	i += 1
--- Занятие5ДЗ/DZ5_4.py
+++ Занятие5ДЗ/DZ5_4.py
@@ -2,8 +2,10 @@
 print('Введите числа x,y')
 x = int(input())
 y = int(input())
-i = 1
-while x < y:
+i = 0
+probeg=0
+while probeg < y:
 	x *= 1.1
 	i += 1
+	probeg += x
 print(i)
